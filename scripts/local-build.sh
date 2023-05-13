2f3dbba0f05c57c168082329b30c014286274c9f
Grillo del Mal <grillo@delmal.cl>
2023-05-13 14:17:35 -0400

    Remove branding assets

---
+++ scripts/local-build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+
+flatpak-builder --default-branch=nightly --force-clean --repo=./repo-dir ./build-dir io.github.grillo_delmal.inochi-creator.yml
+
+flatpak build-bundle \
+    --runtime-repo=https://flathub.org/repo/flathub.flatpakrepo \
+    ./repo-dir \
+    inochi-creator.x86_64.flatpak \
+    io.github.grillo_delmal.inochi-creator nightly
+flatpak build-bundle \
+    --runtime \
+    ./repo-dir \
+    inochi-creator.x86_64.debug.flatpak \
+    io.github.grillo_delmal.inochi_creator.Debug nightly
+
+# flatpak --user -y install inochi-creator.x86_64.flatpak
+# flatpak --user -y install inochi-creator.x86_64.debug.flatpak
