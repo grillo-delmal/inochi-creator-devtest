9dd9251ba7c5cbf5536e7b08a9d00c47323ceb97
Grillo del Mal <grillo@delmal.cl>
2024-09-12 22:33:35 -0300

    Unlock inmath and add numem

--- update-dependencies.sh
+++ update-dependencies.sh
@@ -100,11 +100,10 @@ git clone https://github.com/Inochi2D/i2d-opengl.git
 git clone https://github.com/KitsunebiGames/i18n.git i18n-d
 git clone https://github.com/Inochi2D/dportals.git
 git clone https://github.com/Inochi2D/kra-d.git
+git clone https://github.com/Inochi2D/numem.git
 
 # Fixme Use v0_8 branch until v9 is usable
 git -C ./inochi2d checkout v0_8
-# Lock inmath to v1.0.6 for now ;9
-git -C ./inmath checkout v1.0.6
 
 # Download gitver and semver
 git clone https://github.com/Inochi2D/gitver.git
