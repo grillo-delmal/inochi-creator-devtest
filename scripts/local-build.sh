cb3461c05c7ba68875e0859477c038d4043e8101
Grillo del Mal <grillo@delmal.cl>
2024-09-16 02:59:55 -0300

    Update local build script

--- scripts/local-build.sh
+++ scripts/local-build.sh
@@ -2,18 +2,18 @@
 
 set -e
 
-flatpak-builder --default-branch=nightly --force-clean --repo=./repo-dir ./build-dir io.github.grillo_delmal.inochi-creator.yml
+flatpak-builder --default-branch=localbuild --force-clean --repo=./repo-dir ./build-dir io.github.grillo_delmal.inochi-creator.yml
 
 flatpak build-bundle \
     --runtime-repo=https://flathub.org/repo/flathub.flatpakrepo \
     ./repo-dir \
     inochi-creator.x86_64.flatpak \
-    io.github.grillo_delmal.inochi-creator nightly
+    io.github.grillo_delmal.inochi-creator localbuild
 flatpak build-bundle \
     --runtime \
     ./repo-dir \
     inochi-creator.x86_64.debug.flatpak \
-    io.github.grillo_delmal.inochi_creator.Debug nightly
+    io.github.grillo_delmal.inochi_creator.Debug localbuild
 
 # flatpak --user -y install inochi-creator.x86_64.flatpak
 # flatpak --user -y install inochi-creator.x86_64.debug.flatpak
